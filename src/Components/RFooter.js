e9ba9682c651ec9d49c32516ae8eef223527a46b
Ashutosh Kumar Singh <ashutosh44ks@gmail.com>
2022-06-09 13:33:40 +0530

    add footer in results page

---
+++ src/Components/RFooter.js
@@ -0,0 +1,17 @@
+import "./Stylesheets/HFooter.css";
+
+const RFooter = () => {
+  return (
+    <div className="results-footer">
+      <div className="footer-location">India</div>
+      <ul className="rfooter-main">
+        <li className="hover-underline cursor-pointer">Help</li>
+        <li className="hover-underline cursor-pointer">Send feedback</li>
+        <li className="hover-underline cursor-pointer">Privacy</li>
+        <li className="hover-underline cursor-pointer">Terms</li>
+      </ul>
+    </div>
+  );
+};
+
+export default RFooter;
